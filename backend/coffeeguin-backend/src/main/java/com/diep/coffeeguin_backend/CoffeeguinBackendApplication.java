dbe93c7f9e46bd59bbbc11f4076cacf6d0b1a3eb
Victor Manuel Gonzalez Nahuat <victor@MacBook-Air-de-Victor.local>
2026-05-02 10:46:56 -0600

    Estructura backend y frontend

---
+++ backend/coffeeguin-backend/src/main/java/com/diep/coffeeguin_backend/CoffeeguinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diep.coffeeguin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeguinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeguinBackendApplication.class, args);
+	}
+
+}
---
+++ backend/coffeeguin-backend/src/test/java/com/diep/coffeeguin_backend/CoffeeguinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diep.coffeeguin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeguinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
